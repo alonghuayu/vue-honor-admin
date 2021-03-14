e42d8502eb77620735e69e339143b488f58b45d4
alonghuayu <alonghuayu@163.com>
2021-03-15 00:41:06 +0800

    fix

--- mock/players.ts
+++ mock/players.ts
@@ -1,4 +1,4 @@
 import { Request, Response } from 'express'
-export const getArticle = (req: Request, res: Response) => {
+export const getPlayers = (req: Request, res: Response) => {
     res.json([1, 2, 3])
 }
